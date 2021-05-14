15bf4867c330ac1417206f11a9f707a8d91aeb9a
johnmogi <anguru@gmail.com>
2021-05-14 18:19:45 +0300

    --data

--- Dungeon/src/functions.js
+++ Dungeon/src/functions.js
@@ -1,17 +1,25 @@
-const diceBut = document.getElementById('diceBut')
+fetch("../data/character.JSON")
+.then(res => res.json())
+.then(data => console.log(data))
+
+const diceBut6 = document.getElementById('diceBut6')
 const monsterBut = document.getElementById('monsterBut')
 
+const dice_6 = document.getElementById('dice_6')
+const monsterBoard = document.getElementById('monster-board')
 
-var canvas = document.getElementById("dice_6");
-var ctx = canvas.getContext("2d");
-ctx.font = "50px Arial";
+diceBut6.onclick = function() {drawDice(6)};
+diceBut10.onclick = function() {drawDice(10)};
+diceBut20.onclick = function() {drawDice(20)};
+monsterBut.onclick = function() {generetaMonster()};
 
-diceBut.onclick = function() {drawDice()};
 
+function drawDice(num){
+    `dice_${num}`.innerText = ''   
+    let res = Math.floor(Math.random() * num +1);
+    return dice_6.innerText = res;
+   }
 
-function drawDice(){
-    canvas.width += 0
+   function generetaMonster(){
 
-    let num = Math.floor(Math.random() * 6);
-    return ctx.fillText(num, 10, 50);
    }
