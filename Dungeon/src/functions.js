cd23d0ff3992640f9ccda50c2922cacf6863c52d
johnmogi <anguru@gmail.com>
2021-05-15 11:15:03 +0300

    --chars

--- Dungeon/src/functions.js
+++ Dungeon/src/functions.js
@@ -61,17 +61,16 @@ const lastName = [
   "poka",
   "rigalux",
 ];
-function age() {
-  return;
-}
+const proffession = [  'magician', 'witch', 'monk' ,'warrior',  'thief', 'bard', 'pirate', 'acrobat'
+]
 const chars = {
   boyFirstName: maleFirstNames,
   girlFirstName: femaleFirstNames,
   lastName: lastName,
   type: charsTypes,
   age: genereteNum(),
-  proffession: "",
-  life: "",
+  proffession: proffession,
+  life: '',
   magic: "",
   strength: "",
   rank: "",
@@ -80,6 +79,7 @@ const chars = {
   limitations: "",
   weakness: "",
 };
+
 
 const diceBut6 = document.getElementById("diceBut6");
 const monsterBut = document.getElementById("monsterBut");
@@ -102,8 +102,8 @@ charBut.onclick = function () {
   generetaHero();
 };
 
-function genereteNum() {
-  return Math.floor(Math.random() * 45 + 1);
+function genereteNum(num, base) {
+  return Math.floor(Math.random() * num + base);
 }
 
 function drawDice(num) {
@@ -112,7 +112,6 @@ function drawDice(num) {
   return (dice_6.innerText = res);
 }
 function randomValue(key) {
-  console.log(key);
   return key[
     Object.keys(key)[Math.floor(Math.random() * Object.keys(key).length)]
   ];
@@ -120,14 +119,30 @@ function randomValue(key) {
 function generetaHero() {
   characterBoard.innerText = "";
 const br = '<br/>';
-const charName = 'Character Name: '
+const charName = 'Boy Name: '
 const girlName = 'Girl Name: '
 const lastName = 'Family Name: '
-const charType = 'Character Type: '
-
+const charType = 'Type: '
+const charAge = 'Age: '
+const charPro = 'Proffession: '
+let magicCaster = 
 characterBoard.innerHTML += charName + randomValue(chars.boyFirstName) + br;
 characterBoard.innerHTML += girlName + randomValue(chars.girlFirstName)+ br;
 characterBoard.innerHTML += lastName + randomValue(chars.lastName)+ br;
-characterBoard.innerHTML += charType + randomValue(chars.type)+ br;
-
+characterBoard.innerHTML += charType +  randomValue(chars.type)+ br;
+characterBoard.innerHTML += charAge + genereteNum(75,12) + br;
+characterBoard.innerHTML += charPro + randomValue(chars.proffession) + br;
+characterBoard.innerHTML += "life : " + genereteNum(drawDice(6),6) + br;
+console.log(chars.proffession );
+if (chars.proffession == proffession[0]){
+  
 }
+characterBoard.innerHTML += "Magic : " + genereteNum(drawDice(6),6);
+}
+
+function init(){
+  drawDice(6)
+  generetaHero();
+}
+
+init();
